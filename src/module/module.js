b427b02255184224649ace64da48e3982cce41c7
AMSC30 <930042468@qq.com>
2022-02-28 22:21:57 +0800

    change

--- src/module/module.js
+++ src/module/module.js
@@ -1,71 +1,69 @@
 import { forEachValue } from '../util'
 
-// Base data struct for store's module, package with some attribute and method
+// 根据传进构造函数的配置生成一个新的module，
+// 主要属性：1._children，2._rowModule，3.state
 export default class Module {
-  constructor (rawModule, runtime) {
-    this.runtime = runtime
-    // Store some children item
-    this._children = Object.create(null)
-    // Store the origin module object which passed by programmer
-    this._rawModule = rawModule
-    const rawState = rawModule.state
+    constructor(rawModule, runtime) {
+        this.runtime = runtime
+        this._children = Object.create(null)
+        this._rawModule = rawModule
+        const rawState = rawModule.state
 
-    // Store the origin module's state
-    this.state = (typeof rawState === 'function' ? rawState() : rawState) || {}
-  }
-
-  get namespaced () {
-    return !!this._rawModule.namespaced
-  }
-
-  addChild (key, module) {
-    this._children[key] = module
-  }
-
-  removeChild (key) {
-    delete this._children[key]
-  }
-
-  getChild (key) {
-    return this._children[key]
-  }
-
-  hasChild (key) {
-    return key in this._children
-  }
-
-  update (rawModule) {
-    this._rawModule.namespaced = rawModule.namespaced
-    if (rawModule.actions) {
-      this._rawModule.actions = rawModule.actions
+        this.state = (typeof rawState === 'function' ? rawState() : rawState) || {}
     }
-    if (rawModule.mutations) {
-      this._rawModule.mutations = rawModule.mutations
-    }
-    if (rawModule.getters) {
-      this._rawModule.getters = rawModule.getters
-    }
-  }
 
-  forEachChild (fn) {
-    forEachValue(this._children, fn)
-  }
-
-  forEachGetter (fn) {
-    if (this._rawModule.getters) {
-      forEachValue(this._rawModule.getters, fn)
+    get namespaced() {
+        return !!this._rawModule.namespaced
     }
-  }
 
-  forEachAction (fn) {
-    if (this._rawModule.actions) {
-      forEachValue(this._rawModule.actions, fn)
+    addChild(key, module) {
+        this._children[key] = module
     }
-  }
 
-  forEachMutation (fn) {
-    if (this._rawModule.mutations) {
-      forEachValue(this._rawModule.mutations, fn)
+    removeChild(key) {
+        delete this._children[key]
     }
-  }
+
+    getChild(key) {
+        return this._children[key]
+    }
+
+    hasChild(key) {
+        return key in this._children
+    }
+
+    update(rawModule) {
+        this._rawModule.namespaced = rawModule.namespaced
+        if (rawModule.actions) {
+            this._rawModule.actions = rawModule.actions
+        }
+        if (rawModule.mutations) {
+            this._rawModule.mutations = rawModule.mutations
+        }
+        if (rawModule.getters) {
+            this._rawModule.getters = rawModule.getters
+        }
+    }
+
+    forEachChild(fn) {
+        forEachValue(this._children, fn)
+    }
+
+    forEachGetter(fn) {
+        if (this._rawModule.getters) {
+            forEachValue(this._rawModule.getters, fn)
+        }
+    }
+
+    forEachAction(fn) {
+        if (this._rawModule.actions) {
+            forEachValue(this._rawModule.actions, fn)
+        }
+    }
+
+    forEachMutation(fn) {
+        if (this._rawModule.mutations) {
+            forEachValue(this._rawModule.mutations, fn)
+        }
+    }
 }
